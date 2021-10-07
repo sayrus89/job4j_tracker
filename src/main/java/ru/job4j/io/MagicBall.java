73c269ec1d493673e9d2cbb98d850330b8f105c0
sayrus89 <sayrus5767@mail.ru>
2021-10-07 14:07:05 +0500

    1. Чтение из консоли. Класс Scanner. [#179514]

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Я великий Оракул. Что ты хочешь узнать? ");
+        String question = input.nextLine();
+        int answer = new Random().nextInt(3);
+        if (answer == 0) {
+            System.out.println("Yes");
+        } else if (answer == 1) {
+            System.out.println("No");
+        } else {
+            System.out.println("Maybe");
+        }
+        System.out.println(answer);
+    }
+}
