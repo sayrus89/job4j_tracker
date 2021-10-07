7b131ff60aaea652b5e715be02feed0db2c0d16f
sayrus89 <sayrus5767@mail.ru>
2021-10-07 22:30:14 +0500

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -40,6 +40,7 @@ public class Tracker {
         for (int index = 0; index < size; index++) {
             if (items[index].getId() == id) {
                 rsl = index;
+                break;
             }
         }
         return rsl;
