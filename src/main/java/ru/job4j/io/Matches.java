64c06b7e05f24817b87d618ace091f32bc6b3ac9
sayrus89 <sayrus5767@mail.ru>
2021-10-09 17:46:15 +0500

    2. Scanner и чтение числа из консоли. [#179518 #213269] исправление 2

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -16,6 +16,8 @@ public class Matches {
             turn = !turn;
                 count = count - matches;
                 System.out.println(count);
+            } else {
+                System.out.println("Ошибка вы ввели неверное число. Введите число от 1 до 3.");
             }
         }
         if (!turn) {
