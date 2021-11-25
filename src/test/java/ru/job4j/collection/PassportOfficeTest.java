bb2fea30368b29e6477dce81aa7d16766da2915d
sayrus89 <sayrus5767@mail.ru>
2021-11-25 20:42:31 +0500

    1. Паспорт и жители [#212656 #232656] исправление 2.

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -21,6 +21,6 @@ public class PassportOfficeTest {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
         PassportOffice office = new PassportOffice();
         office.add(citizen);
-        assertThat(office.add(new Citizen("2f44a", "Petr Arsentev")), is(false));
+        assertFalse(office.add(citizen));
     }
 }
